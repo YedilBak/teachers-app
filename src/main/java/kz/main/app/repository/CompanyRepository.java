240ad9f1d30fcb0b4314f52a7a86a2fe360e1582
yedil.bakenov <e.bakenov@kpecom.kz>
2026-03-13 20:11:05 +0500

    add files

---
+++ src/main/java/kz/main/app/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package kz.main.app.repository;
+
+import kz.main.app.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
+}
---
+++ src/main/java/kz/main/app/repository/CoursesRepository.java
@@ -0,0 +1,9 @@
+package kz.main.app.repository;
+
+import kz.main.app.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoursesRepository extends JpaRepository<Course, Integer> {
+}
